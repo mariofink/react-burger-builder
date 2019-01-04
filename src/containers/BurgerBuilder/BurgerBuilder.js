2f6b2b3dcc2d676221021638c70fe4d818ee72ae
Mario Fink <mario.fink@goodyear.com>
2019-01-04 11:22:11 +0100

    add changeable ingredients list

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,8 +3,27 @@ import React, { Component } from "react";
 export default class BurgerBuilder extends Component {
   constructor() {
     super();
+    this.state = {
+      ingredients: []
+    };
+  }
+  addIngredient(ingredient) {
+    this.setState({
+      ingredients: [ingredient, ...this.state.ingredients]
+    });
   }
   render() {
-    return <div>BurgerBuilder</div>;
+    return (
+      <div>
+        <div>{this.state.ingredients}</div>
+        <div>
+          <button onClick={() => this.addIngredient("bread")}>Add bread</button>
+          <button onClick={() => this.addIngredient("patty")}>Add patty</button>
+          <button onClick={() => this.addIngredient("tomatoe")}>
+            Add tomatoe
+          </button>
+        </div>
+      </div>
+    );
   }
 }
